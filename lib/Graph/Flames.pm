6043eb8ca82e82b393ebdc01e7a002a418f89e73
Erik Carlsson <info@code301.com>
2016-09-19 08:05:24 +0000

    Reduce filesize by refering names to other blobs
    The first blob with a specific namespace, subroutine name or filename (for __ANON__ stuff)
    gets this set in data attributes. Later when other blobs have the same name
    it refers to that first element. This saves many percent of bytes (10-20%).
    <text> and <title> can be dynamically inserted as needed, client side.

--- lib/Graph/Flames.pm
+++ lib/Graph/Flames.pm
@@ -166,6 +166,34 @@ has converter => (
 sub _build_converter($self) {
     Convert::AnyBase->new(set => join '', (0..9, 'a'..'z', 'A'..'Z'));
 }
+has stack_class => (
+    is => 'rw',
+    isa => Int,
+    init_arg => undef,
+    default => 1,
+);
+sub incr_stack_class($self) {
+    my $stack_class = $self->stack_class + 1;
+    $self->stack_class($stack_class);
+    return $stack_class;
+}
+# this makes it possible to have every unique namespace in only one element, and the rest
+# fetches the name from that. Similar for sub name and __ANON__ stuff
+has stack_ns_referer => (
+    is => 'rw',
+    isa => HashRef,
+    default => sub { +{ } },
+);
+has stack_sn_referer => (
+    is => 'rw',
+    isa => HashRef,
+    default => sub { +{ } },
+);
+has stack_fn_referer => (
+    is => 'rw',
+    isa => HashRef,
+    default => sub { +{ } },
+);
 
 sub _build_svg($self) {
 
@@ -182,30 +210,56 @@ sub _build_svg($self) {
     my $x = $self->svg_config->{'width'};
     my $y = (2 + $self->max_depth) * $self->flame_config->{'depth_height'} - $self->flame_config->{'depth_height'};
 
+    my $stack_classes = ['1'];
+    $self->draw($chaing, $self->callchain, $x, $y, $stack_classes);
 
-    $self->draw($svg, $self->callchain, $x, $y);
     return $svg;
 }
 
 # $x is the *right* hand x..
-sub draw($self, $svg, $chain, $x, $y) {
+sub draw($self, $svg, $chain, $x, $y, $stack_ids) {
     my $width = sprintf '%.3f' => $chain->total_ticks / $self->ticks_per_pixel;
     my $seconds = $chain->total_ticks / $self->ticks_per_second;
     my $percent = sprintf '%.2f' => $chain->total_ticks / $self->total_ticks * 100;
     my $ms = format_number(int($seconds * 1_000_000));
-    my @classes = ('chain', ($self->color_class_for_name($chain->name) || 'chain color-0'));
-    push @classes => 'zoom-too-thin' if $width < 0.1;
+
     my $id = 's-'.$self->converter->encode($stack_ids->[-1]);
+    my @classes = ($self->color_class_for_name($chain->name) || 'c-0');
+    push @classes => 'zoom-too-thin' if $width < 0.3;
     push @classes => map { 's-'.$self->converter->encode($_) } $stack_ids->@*;
+
+    my %referattr;
+    if($chain->name) {
+        $chain->name =~ m{^ (?<ns>.+) :: (?<sn>.*?) (?: __ANON__\[ (?<fn>.*):(?<num>\d+)\] )? $}x;
+
+        # namespace subname filename
+        for my $type (qw/ns sn fn/) {
+            my $method = sprintf 'stack_%s_referer', $type;
+
+            if(defined $+{ $type } && length $+{ $type }) {
+                if(exists $self->$method->{ $+{ $type } }) {
+                    # data-<type> attributes refer to data-r<type>..
+                    $referattr{ "data-$type" } = $self->$method->{ $+{ $type } };
+                }
+                else {
+                    # data-r<type> attributes hold the real value
+                    $referattr{ "data-r$type" } = $+{ $type };
+                    $self->$method->{ $+{ $type } } = $id;
+                }
+            }
+        }
+        $referattr{'data-num'} = $+{'num'} if $+{'num'};
+    }
 
     my $g = $svg->tag(g =>
         class => join (' ' => @classes),
-        'data-ticks' => $chain->total_ticks,
+        'data-t' => $chain->total_ticks,
         'data-ms' => $ms,
-        'data-percent' => $percent,
-        'data-name' => $chain->name,
+        'data-pc' => $percent,
+        id => $id,
+        %referattr,
     );
-    $g->title(-cdata => sprintf '%s (%s microseconds, %s%%)', $chain->name, $ms, $percent);
+   # $g->title(-cdata => sprintf '%s (%s microseconds, %s%%)', $chain->name, $ms, $percent);
 
     $g->rectangle(x => $x - $width,
                   y => $y,
@@ -228,11 +282,15 @@ sub draw($self, $svg, $chain, $x, $y) {
                  -cdata => $text,
         );
     }
+    # The *first* line in <g> is expected to be the right border (saves setting a css class)
+    $g->line(x1 => $x, y1 => $y, x2 => $x, y2 => $y + $self->flame_config->{'depth_height'});
 
     my $child_x = $x;
     my $child_y = $y - $self->flame_config->{'depth_height'};
+
     for my $child (sort { $b->name cmp $a->name } $chain->children->@*) {
-        $child_x -= $self->draw($svg, $child, $child_x, $y = $child_y - 1);
+        my $child_stack_id = $self->incr_stack_class;
+        $child_x -= $self->draw($svg, $child, $child_x, $y = $child_y - 1, [$stack_ids->@*, $child_stack_id]);
     }
     return $width;
 }
@@ -255,3 +313,4 @@ sub calculate_name_color_group($self, $name) {
 }
 
 1;
+
